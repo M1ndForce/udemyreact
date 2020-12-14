dcb232dd7330dac8f9c501d462adb9290dffcb04
M1ndForce <66359796+M1ndForce@users.noreply.github.com>
2020-12-14 18:56:53 +0200

    class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import React, {  useState } from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
+import { render } from "@testing-library/react";
 
-const App = (props) => {
-  // setPersonsState() change state but not merge it
-  const [personState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: "Max", age: 28 },
       { name: "Manu", age: 29 },
@@ -12,13 +12,11 @@ const App = (props) => {
       { action: false },
     ],
     otherState: "some other value",
-  });
+  };
 
-  const [otherState, changeOtherState] = useState("Hi all");
-
-  const switchNameHandler = () => {
-    if (personState.persons[3].action === false) {
-      setPersonsState({
+  switchNameHandler = () => {
+    if (this.state.persons[3].action === false) {
+      this.setState({
         persons: [
           { name: "ololosha", age: 33 },
           { name: "Manu", age: 29 },
@@ -26,8 +24,8 @@ const App = (props) => {
           { action: true },
         ],
       });
-    } else if (personState.persons[3].action === true) {
-      setPersonsState({
+    } else if (this.state.persons[3].action === true) {
+      this.setState({
         persons: [
           { name: "Max", age: 28 },
           { name: "Manu", age: 29 },
@@ -38,66 +36,36 @@ const App = (props) => {
     }
   };
 
-  return (
-    <div className="App">
-      <h1> Hello world </h1>
-      <Person
-        name={personState.persons[0].name}
-        age={personState.persons[0].age}
-      >
-        olololololo
-      </Person>
-      <Person
-        name={personState.persons[1].name}
-        age={personState.persons[1].age}
-      />
-      <Person
-        name={personState.persons[2].name}
-        age={personState.persons[2].age}
-      >
-        OLOLOLOLO HI World {otherState}
-      </Person>
-      <button onClick={switchNameHandler}>Switch Name</button>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1> Hello world </h1>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          olololololo
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        >
+          <h1>OLOLOLOLO HI World</h1>
+        </Person>
+        <button>Switch Name</button>
+      </div>
+    );
 
-  // return React.createElement(
-  // "div",
-  // null,
-  // React.createElement("h1", { className: "App" }, "Does this work now!")
-  // );
-};
+    // return React.createElement(
+    // "div",
+    // null,
+    // React.createElement("h1", { className: "App" }, "Does this work now!")
+    // );
+  }
+}
 
 export default App;
-// class component
-//  state = {
-//     persons: [
-//       { name: "Max", age: 28 },
-//       { name: "Manu", age: 29 },
-//       { name: "Stephanie", age: 26 },
-//       { action: false },
-//     ],
-//     otherState: "some other value",
-//   };
-//
-//   switchNameHandler = () => {
-//     if (this.state.persons[3].action === false) {
-//       this.setState({
-//         persons: [
-//           { name: "ololosha", age: 33 },
-//           { name: "Manu", age: 29 },
-//           { name: "Stephanienenka", age: 111111111111111 },
-//           { action: true },
-//         ],
-//       });
-//     } else if (this.state.persons[3].action === true) {
-//       this.setState({
-//         persons: [
-//           { name: "Max", age: 28 },
-//           { name: "Manu", age: 29 },
-//           { name: "Stephanie", age: 26 },
-//           { action: false },
-//         ],
-//       });
-//     }
-//   };
